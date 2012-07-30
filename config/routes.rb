babcf4b5225a505bfd99c41802293b82e83f768a
AndrewHulbert <ahulbert7@gmail.com>
2012-07-30 12:19:20 -0500

    Extra Examples in Chapter 5

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,13 @@ SampleApp::Application.routes.draw do
 
   root to: 'static_pages#home'
 
-  match '/help', to: 'static_pages#help'
+  match 'help', to: 'static_pages#help'
 
-  match '/about', to: 'static_pages#about'
+  match 'about', to: 'static_pages#about'
 
-  match '/contact', to: 'static_pages#contact'
+  match 'contact', to: 'static_pages#contact'
 
-  match '/signup', to: 'users#new'
+  match 'signup', to: 'users#new'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
