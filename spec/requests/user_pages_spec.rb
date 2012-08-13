f2ba9b1e14e8718679675cf96625b6abc5295281
AndrewHulbert <ahulbert7@gmail.com>
2012-08-13 14:05:43 -0500

    Finish user edit, update, index, and destroy actions

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,6 +4,54 @@ describe "UserPages" do
 	
 	subject { page }
 
+  describe "index" do
+
+    let(:user) { FactoryGirl.create(:user) }
+
+    before(:all) { 30.times { FactoryGirl.create(:user) } }
+    after(:all)  { User.delete_all }
+
+    before(:each) do
+      visit signin_path
+      sign_in user
+      visit users_path
+    end
+
+    it { should have_selector('title', text: 'All users') }
+    it { should have_selector('h1',    text: 'All users') }
+
+    describe "pagination" do
+
+      it { should have_selector('div.pagination') }
+
+      it "should list each user" do
+        User.paginate(page: 1).each do |user|
+          page.should have_selector('li', text: user.name)
+        end
+      end
+    end
+
+    describe "delete links" do
+
+      it { should_not have_link('delete') }
+
+      describe "as an admin user" do
+        let(:admin) { FactoryGirl.create(:admin) }
+        before do
+          visit signin_path
+          sign_in admin
+          visit users_path
+        end
+
+        it { should have_link('delete', href: user_path(User.first)) }
+        it "should be able to delete another user" do
+          expect { click_link('delete') }.to change(User, :count).by(-1)
+        end
+        it { should_not have_link('delete', href: user_path(admin)) }
+      end
+    end
+  end
+
 	describe "profile page" do
 		before { visit user_path(user) }
 		let(:user) {FactoryGirl.create(:user) }
@@ -13,20 +61,20 @@ describe "UserPages" do
 		it { should have_selector('title', text: user.name) }
 	end
   
-  	describe "signup page" do
-  		before {visit signup_path}
-    	it { should have_selector('h1', text: 'Sign up') }
-    	it { should have_selector('title', text: full_title('Sign up'))}
-  	end
+  describe "signup page" do
+  	before {visit signup_path}
+  	it { should have_selector('h1', text: 'Sign up') }
+   	it { should have_selector('title', text: full_title('Sign up'))}
+  end
 
-  	describe "signup" do
+    describe "signup" do
   		before { visit signup_path}
   		let(:submit) { "Create my account"}
 
   		describe "with invalid information" do
   			it "should not create a user" do
   				expect { click_button submit }.not_to change(User, :count)
-  			end
+  		end
 
         describe "after submission" do
           before { click_button submit }
@@ -34,28 +82,95 @@ describe "UserPages" do
           it { should have_selector('title', text: 'Sign up') }
           it { should have_content('error') }
         end
-  		end
+  	end
 
-  		describe "with valid information" do
+  	describe "with valid information" do
   			before do
-  				fill_in "Name", with: "Exmpale User"
+  				fill_in "Name", with: "Exmaple User"
   				fill_in "Email", with: "user@example.com"
   				fill_in "Password", with: "foobar"
   				fill_in "Password confirmation", with: "foobar"
-  			end
-
-  			it "should create a user" do
-  				expect { click_button submit }.to change(User, :count).by(1)
-  			end
-
-        describe "after saving the user" do
-          before { click_button submit }
-          let(:user) { User.find_by_email('user@example.com') }
-
-          it { should have_selector('title', text: user.name) }
-          it { should have_selector('div.alert.alert-success', text: 'Welcome') }
-          it { should have_link('Sign out') }
-        end
   		end
+
+  		it "should create a user" do
+  			expect { click_button submit }.to change(User, :count).by(1)
+  		end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_link('Sign out') }
+      end
   	end
+  end
+
+
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before do
+      visit signin_path
+      sign_in user
+      visit edit_user_path(user)
+    end
+
+    describe "page" do
+      it { should have_selector('h1',    text: "Update your profile") }
+      it { should have_selector('title', text: "Edit user") }
+      it { should have_link('change', href: 'http://gravatar.com/emails') }
+    end
+
+    describe "with invalid information" do
+      before { click_button "Save changes" }
+      it { should have_content('error') }
+    end
+
+    describe "with valid information" do
+      let(:new_name)  { "New Name" }
+      let(:new_email) { "new@example.com" }
+      before do
+        fill_in "Name",             with: new_name
+        fill_in "Email",            with: new_email
+        fill_in "Password",         with: user.password
+        fill_in "Confirm Password", with: user.password
+        click_button "Save changes"
+      end
+
+      it { should have_selector('title', text: new_name) }
+      it { should have_selector('div.alert.alert-success') }
+      it { should have_link('Sign out', href: signout_path) }
+      specify { user.reload.name.should  == new_name }
+      specify { user.reload.email.should == new_email }
+    end
+  end
+
+  describe "following/followers" do
+    let(:user) { FactoryGirl.create(:user) }
+    let(:other_user) { FactoryGirl.create(:user) }
+    before { user.follow!(other_user) }
+
+    describe "followed users" do
+      before do
+        sign_in user
+        visit following_user_path(user)
+      end
+
+      it { should have_selector('title', text: full_title('Following')) }
+      it { should have_selector('h3', text: 'Following') }
+      it { should have_link(other_user.name, href: user_path(other_user)) }
+    end
+
+    describe "followers" do
+      before do
+        sign_in other_user
+        visit followers_user_path(other_user)
+      end
+
+      it { should have_selector('title', text: full_title('Followers')) }
+      it { should have_selector('h3', text: 'Followers') }
+      it { should have_link(user.name, href: user_path(user)) }
+    end
+  end
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,5 +1,7 @@
 include ApplicationHelper
 
+
+
 def valid_signin(user)
 	fill_in "Email", with: user.email
 	fill_in "Password", with: user.password
@@ -10,4 +12,13 @@ RSpec::Matchers.define :have_error_message do |message|
 	match do |page|
 		page.should have_selector('div.alert.alert-error', text: message)
 	end
+end
+
+def sign_in(user)
+	visit signin_path
+	fill_in "Email", with: user.email
+	fill_in "Password", with: user.password
+	click_button "Sign in"
+	# Sign in when not using Capybara as well.
+	cookies[:remember_token] = user.remember_token
 end
